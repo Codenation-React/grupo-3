add8324cbbb0b5b868c81ce699b980fbfb641e82
Vicente Santos <vicentedpsantos@gmail.com>
2020-06-27 19:02:25 -0300

    Move state manager to new app

---
+++ src/state/ducks/cart/operations.js
@@ -0,0 +1,15 @@
+import {
+  addToCart,
+  changeQuantity,
+  removeFromCart,
+  clearCart,
+  setCart,
+} from "./actions";
+
+export {
+  addToCart,
+  changeQuantity,
+  removeFromCart,
+  clearCart,
+  setCart
+};
---
+++ src/state/ducks/cart/selectors.js
@@ -0,0 +1,3 @@
+export function getCartItemQuantity(cart, id) {
+  return cart.find(item => item.product.id === id).quantity;
+}
