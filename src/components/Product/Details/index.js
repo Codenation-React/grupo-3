a66cda577c68903619279d10a2f0f742f61888e1
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-07-04 17:51:05 +0100

    Styled product details' component.

--- src/components/Product/Details/index.js
+++ src/components/Product/Details/index.js
@@ -1,27 +1,47 @@
 // @format
-import React from "react";
+import React, { useState } from "react";
 import Card from "../Card";
 import { connect } from "react-redux";
 import { v1 as uuid } from "uuid";
 
+import "./styles.css";
+
 const Controls = ({ product }) => {
   const { sizes, regular_price, name, installments } = product[0];
+  const [isSelected, setSelected] = useState("");
 
   return (
-    <div>
-      <h1>{name}</h1>
+    <div className="product__content">
+      <h3 className="product__name">{name}</h3>
       <div>
-        <span>{regular_price}</span>
-        <span>{installments}</span>
+        <span className="product__price product__price--to">
+          {regular_price}
+        </span>
+        <span className="product__price product__price--installments">
+          {installments}
+        </span>
       </div>
-
-      <div>Escolha o tamanho:</div>
-      <div>{sizes && sizes.map(size => <span>{size.size}</span>)}</div>
+      <div className="product__sizes">
+        <p className="product__sizes__title">Escolha o tamanho:</p>
+        <div className="product__btn-group">
+          {sizes &&
+            sizes.map((size) => (
+              <button
+                className={`product__filter ${
+                  isSelected === size.size ? "product__filter--selected" : ""
+                }`}
+                onClick={() => setSelected(size.size)}
+              >
+                {size.size}
+              </button>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-const Details = props => {
+const Details = (props) => {
   const { state, product } = props;
   const style = props.match.params.style;
 
@@ -34,7 +54,7 @@ const Details = props => {
 };
 
 const mapStateToProps = (state, ownProps) => {
-  const product = state.product.list.filter(product => {
+  const product = state.product.list.filter((product) => {
     return product.style === ownProps.match.params.style;
   });
 
